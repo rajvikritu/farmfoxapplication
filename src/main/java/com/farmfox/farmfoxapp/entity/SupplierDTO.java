cf6b80a971be2fce8dedb0fd2d3ae6401c04a758
rituraj <riturajpanapur@gmail.com>
2026-03-26 06:28:57 -0700

    added relevant company details for supplier

--- src/main/java/com/farmfox/farmfoxapp/entity/SupplierDTO.java
+++ src/main/java/com/farmfox/farmfoxapp/entity/SupplierDTO.java
@@ -2,13 +2,12 @@ package com.farmfox.farmfoxapp.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class SupplierDTO {
-    private String supplierName;
-    private String productName;
+    private String companyName;
+    private String companyAddress;
     private String mfgDate;
     private String lotNo;
 }
